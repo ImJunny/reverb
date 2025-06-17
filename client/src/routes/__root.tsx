bd7e99c925c73027bb5384e2efbe5ec9d5dc0513
John Nguyen <johnnguyen4501@gmail.com>
2025-06-17 00:27:44 -0400

    add theme provider

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,9 +1,10 @@
+import { ThemeProvider } from "@/components/theme-provider";
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 export const Route = createRootRoute({
   component: () => (
-    <>
+    <ThemeProvider>
       <div className="p-2 flex gap-2">
         <Link to="/" className="[&.active]:font-bold">
           Home
@@ -15,6 +16,6 @@ export const Route = createRootRoute({
       <hr />
       <Outlet />
       <TanStackRouterDevtools />
-    </>
+    </ThemeProvider>
   ),
 });
