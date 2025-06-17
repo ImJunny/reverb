f2c6ff5af5471e8bb1408dfb69e6c87e6d343578
John Nguyen <johnnguyen4501@gmail.com>
2025-06-17 00:24:58 -0400

    init

---
+++ client/vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+
+export default defineConfig({
+  plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      "@client": path.resolve(__dirname, "./src"),
+      "@server": path.resolve(__dirname, "../server/src"),
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
